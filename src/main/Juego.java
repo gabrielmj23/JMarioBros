18346a40caa470a031da1ca698926fa913754d00
gabrielmj23 <gabrielmj2312@gmail.com>
2023-06-07 10:19:40 -0400

    Dibujar en cada frame y limitar a 120FPS

--- src/main/Juego.java
+++ src/main/Juego.java
@@ -4,14 +4,36 @@ package main;
  *
  * @author Gabriel
  */
-public class Juego {
+public class Juego implements Runnable {
 
     private VentanaJuego ventana;
     private PanelJuego panel;
+    private Thread hiloJuego;
+    private final int FPS_FIJOS = 120;
 
     public Juego() {
         panel = new PanelJuego();
         ventana = new VentanaJuego(panel);
         panel.requestFocus();
+        iniciarJuego();
+    }
+
+    private void iniciarJuego() {
+        hiloJuego = new Thread(this);
+        hiloJuego.start();
+    }
+
+    @Override
+    public void run() {
+        long ultimoFrame = System.nanoTime();
+        final double TIEMPO_POR_FRAME = 1000000000.0 / FPS_FIJOS;
+
+        while (true) {
+            long ahora = System.nanoTime();
+            if (ahora - ultimoFrame >= TIEMPO_POR_FRAME) {
+                panel.repaint();
+                ultimoFrame = ahora;
+            }
+        }
     }
 }
