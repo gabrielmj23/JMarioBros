54ccc9fe2fb67e5e53c85b06fda9c628f890464c
gabrielmj23 <gabrielmj2312@gmail.com>
2023-06-16 21:38:52 -0400

    Modificar ciclo de juego diferenciando frames y actualizaciones

--- src/main/Juego.java
+++ src/main/Juego.java
@@ -10,6 +10,7 @@ public class Juego implements Runnable {
     private PanelJuego panel;
     private Thread hiloJuego;
     private final int FPS_FIJOS = 120;
+    private final int UPS_FIJOS = 200;
 
     public Juego() {
         panel = new PanelJuego();
@@ -22,17 +23,39 @@ public class Juego implements Runnable {
         hiloJuego = new Thread(this);
         hiloJuego.start();
     }
+    
+    /**
+     * Gestiona actualizaciones de objetos (personajes, paneles, etc)
+     */
+    public void actualizar() {
+        panel.actualizarJuego();
+    }
 
+    /**
+     * Gestiona el ciclo de juego. Por mantiene 120 frames por segundo y 200
+     * actualizaciones por segundo
+     */
     @Override
     public void run() {
-        long ultimoFrame = System.nanoTime();
         final double TIEMPO_POR_FRAME = 1000000000.0 / FPS_FIJOS;
+        final double TIEMPO_POR_ACTUALIZAR = 1000000000.0 / UPS_FIJOS;
+        long tiempoPrevio = System.nanoTime();
+        // Las variables delta acumulan la fraccion de tiempo restante antes de cambiar de frame/actualizar
+        double deltaActualizar = 0;
+        double deltaFrames = 0;
 
         while (true) {
-            long ahora = System.nanoTime();
-            if (ahora - ultimoFrame >= TIEMPO_POR_FRAME) {
+            long tiempoAhora = System.nanoTime();
+            deltaActualizar += (tiempoAhora - tiempoPrevio) / TIEMPO_POR_ACTUALIZAR;
+            deltaFrames += (tiempoAhora - tiempoPrevio) / TIEMPO_POR_FRAME;
+            tiempoPrevio = tiempoAhora;
+
+            if (deltaActualizar >= 1) {
+                actualizar();
+                deltaActualizar--;
+            }
+            if (deltaFrames >= 1) {
                 panel.repaint();
-                ultimoFrame = ahora;
             }
         }
     }
--- src/main/PanelJuego.java
+++ src/main/PanelJuego.java
@@ -7,8 +7,15 @@ import javax.swing.JPanel;
  * @author Gabriel
  */
 public class PanelJuego extends JPanel {
-    
+
     public PanelJuego() {
-        
+
+    }
+
+    /**
+     * Gestiona actualizaciones referentes a animaciones y objetos en el panel
+     */
+    public void actualizarJuego() {
+
     }
 }
