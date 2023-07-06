f7757550f340396693db6307c8d770186ec74050
José Andrés Bertorelli <107157202+Mostroso2003@users.noreply.github.com>
2023-07-06 01:45:47 -0400

    Add files via upload

---
+++ src/estadojuego/MetodosDeEstados.java
@@ -0,0 +1,25 @@
+package estadojuego;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+
+public interface MetodosDeEstados {
+    public void actualizar();
+    
+    public void dibujar(Graphics g);
+    
+    /* EN CASO DE USAR EL MOUSE, VER #11
+    public void mouseClicked();
+    
+    public void mousePressed();
+    
+    public void mouseReleased();
+    
+    public void mouseMoved();    
+    */
+    public void keyPressed(KeyEvent e);
+    
+    public void keyReleased(KeyEvent e);
+    
+}
